e942da7feae8d9ea71b1474c39f14d26fae6257e
Alireza Nezami <alireza.nezami@solidict.com>
2023-07-05 15:42:40 +0330

    fix: change the output of the api request to the new models

--- core/network/src/main/java/alireza/nezami/network/api/NetworkApi.kt
+++ core/network/src/main/java/alireza/nezami/network/api/NetworkApi.kt
@@ -1,6 +1,10 @@
 package alireza.nezami.network.api
 
 import alireza.nezami.network.model.NetworkResponse
+import alireza.nezami.network.model.genre.GetGenreListResponseDto
+import alireza.nezami.network.model.movie.GetMovieResponseDto
+import alireza.nezami.network.model.movieDetial.GetMovieDetailResponseDto
+import alireza.nezami.network.model.search.SearchMovieResponseDto
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -10,35 +14,35 @@ interface NetworkApi {
     @GET("movie/now_playing")
     suspend fun getNowPlaying(
         @Query("page") page: Int?,
-    ): NetworkResponse<List<Nothing>>
+    ): NetworkResponse<GetMovieResponseDto>
 
     @GET("movie/popular")
     suspend fun getPopular(
         @Query("page") page: Int?,
-    ): NetworkResponse<List<Nothing>>
+    ): NetworkResponse<GetMovieResponseDto>
 
     @GET("movie/top_rated")
     suspend fun getTopRated(
         @Query("page") page: Int?,
-    ): NetworkResponse<List<Nothing>>
+    ): NetworkResponse<GetMovieResponseDto>
 
     @GET("movie/upcoming")
     suspend fun getUpcoming(
         @Query("page") page: Int?,
-    ): NetworkResponse<List<Nothing>>
+    ): NetworkResponse<GetMovieResponseDto>
 
     @GET("genre/movie/list")
-    suspend fun getGenreList(): NetworkResponse<List<Nothing>>
+    suspend fun getGenreList(): NetworkResponse<GetGenreListResponseDto>
 
     @GET("movie/{id}")
     suspend fun getMovieDetail(
         @Path("id") movieId: Int,
-    ): NetworkResponse<List<Nothing>>
+    ): NetworkResponse<GetMovieDetailResponseDto>
 
     @GET("search/movie")
     suspend fun searchMovies(
         @Query("page") page: Int?,
         @Query("query") query: String?,
-    ): NetworkResponse<List<Nothing>>
+    ): NetworkResponse<SearchMovieResponseDto>
 
 }
