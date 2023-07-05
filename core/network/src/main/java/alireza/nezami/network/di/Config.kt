828cac7cb7612edb62a42e1ef857658719b71c43
Alireza Nezami <alireza.nezami@solidict.com>
2023-07-05 15:00:20 +0330

    fix: make config variables const

--- core/network/src/main/java/alireza/nezami/network/di/Config.kt
+++ core/network/src/main/java/alireza/nezami/network/di/Config.kt
@@ -1,6 +1,6 @@
 package alireza.nezami.network.di
 
 object Config {
-    val API_KEY = "0e7274f05c36db12cbe71d9ab0393d47"
-    val BACKEND_URL = "https://api.themoviedb.org/3"
+    const val API_KEY = "0e7274f05c36db12cbe71d9ab0393d47"
+    const val BACKEND_URL = "https://api.themoviedb.org/3/"
 }
