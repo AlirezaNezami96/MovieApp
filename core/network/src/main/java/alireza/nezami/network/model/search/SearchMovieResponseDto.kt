401bbdc879e16c230e2bd87839f753af3adac49d
Alireza Nezami <alireza.nezami@solidict.com>
2023-07-06 15:13:33 +0330

    feat: add asExternalModel to searchDto

--- core/network/src/main/java/alireza/nezami/network/model/search/SearchMovieResponseDto.kt
+++ core/network/src/main/java/alireza/nezami/network/model/search/SearchMovieResponseDto.kt
@@ -1,7 +1,10 @@
 package alireza.nezami.network.model.search
 
 
+import alireza.nezami.common.extensions.orZero
+import alireza.nezami.model.search.Search
 import alireza.nezami.network.model.movie.MovieResultDto
+import alireza.nezami.network.model.movie.asExternalModel
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
@@ -16,3 +19,12 @@ data class SearchMovieResponseDto(
     @SerialName("total_results")
     val totalResults: Int? = null
 )
+
+fun SearchMovieResponseDto?.asExternalModel(): Search {
+    return Search(
+        page = this?.page.orZero(),
+        results = this?.results?.mapNotNull { it?.asExternalModel() } ?: emptyList(),
+        totalPages = this?.totalPages.orZero(),
+        totalResults = this?.totalResults.orZero()
+    )
+}
