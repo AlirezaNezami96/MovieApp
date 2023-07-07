40b8f655d8aa526b9aef98dc0636ddfa4d6c9cc7
Alireza Nezami <alireza.nezami@solidict.com>
2023-07-07 18:08:49 +0330

    fix: write comments for extension functions

--- core/common/src/main/java/alireza/nezami/common/extensions/Extensions.kt
+++ core/common/src/main/java/alireza/nezami/common/extensions/Extensions.kt
@@ -1,13 +1,28 @@
 package alireza.nezami.common.extensions
 
+/**
+ * Returns this [Int] value if it is not null, or 0 if it is null.
+ *
+ * @return The non-null [Int] value or 0.
+ */
 fun Int?.orZero() : Int {
     return this ?: 0
 }
 
+/**
+ * Returns this [Double] value if it is not null, or 0.0 if it is null.
+ *
+ * @return The non-null [Double] value or 0.0.
+ */
 fun Double?.orZero(): Double {
     return this ?: 0.0
 }
 
+/**
+ * Returns this [Boolean] value if it is not null, or false if it is null.
+ *
+ * @return The non-null [Boolean] value or false.
+ */
 fun Boolean?.orFalse(): Boolean {
     return this ?: false
 }
