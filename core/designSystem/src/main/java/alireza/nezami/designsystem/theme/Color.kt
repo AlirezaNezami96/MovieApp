89011767f25f8ec91fd4aea3b4058cf0bf82c5ed
Alireza Nezami <alireza.nezami@solidict.com>
2023-07-09 12:16:44 +0330

    feat: add Typography, Color and Icon tint provider

---
+++ core/designSystem/src/main/java/alireza/nezami/designsystem/theme/Color.kt
@@ -0,0 +1,10 @@
+package alireza.nezami.designsystem.theme
+
+import androidx.compose.ui.graphics.Color
+
+internal val DarkPurple90 = Color(0xFF0B0D2C)
+internal val DarkBlue60 = Color(0xFF2F3354)
+internal val White70 = Color(0xFFFFFFFF)
+internal val Orange80 = Color(0xFFFFB800)
+internal val Blue70 = Color(0xFF0296E5)
+
