f22b3d0d5888e7498eff6bab656b100acf448306
Alireza Nezami <alireza.nezami@solidict.com>
2023-07-09 12:20:23 +0330

    fix: change TopLevelDestination to have titleTextId for topBar title

--- app/src/main/java/alireza/nezami/movieapp/navigation/TopLevelDestination.kt
+++ app/src/main/java/alireza/nezami/movieapp/navigation/TopLevelDestination.kt
@@ -10,17 +10,21 @@ import androidx.compose.ui.graphics.vector.ImageVector
 enum class TopLevelDestination(
     val selectedIcon: ImageVector,
     val iconTextId: Int,
+    val titleTextId: Int,
 ) {
     HOME(
         selectedIcon = Icons.Rounded.Home,
         iconTextId = R.string.home,
+        titleTextId = R.string.home
     ),
     SEARCH(
         selectedIcon = Icons.Rounded.Search,
         iconTextId = R.string.search,
+        titleTextId = R.string.search,
     ),
     FAVORITE(
         selectedIcon = Icons.Rounded.Favorite,
         iconTextId = R.string.favorite,
+        titleTextId = R.string.favorite,
     ),
 }
