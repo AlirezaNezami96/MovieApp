936f16d5c64ee1026d6ea396b4b62bc8dd9c25b2
Alireza Nezami <alireza.nezami@solidict.com>
2023-07-10 16:41:55 +0330

    fix: change the type of the movies to list, add state to each tab

--- features/home/src/main/java/alireza/nezami/home/presentation/movieList/contract/MoviesUiState.kt
+++ features/home/src/main/java/alireza/nezami/home/presentation/movieList/contract/MoviesUiState.kt
@@ -1,5 +1,7 @@
 package alireza.nezami.home.presentation.movieList.contract
 
+import alireza.nezami.model.movie.Movie
+import alireza.nezami.model.movie.MovieState
 import alireza.nezami.model.movie.Movies
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
@@ -8,12 +10,17 @@ import javax.annotation.concurrent.Immutable
 @Immutable
 @Parcelize
 data class MoviesUiState(
-    val topRatedMovies: Movies = Movies.EMPTY,
-    val upcomingMovies: Movies = Movies.EMPTY,
-    val popularMovies: Movies = Movies.EMPTY,
-    val nowPlayingMovies: Movies = Movies.EMPTY,
+    val topRatedMovies: List<Movie> = emptyList(),
+    val upcomingMovies: List<Movie> = emptyList(),
+    val popularMovies: List<Movie> = emptyList(),
+    val nowPlayingMovies: List<Movie> = emptyList(),
 
-    val selectedTabIndex : Int = MoviesTabState.NowPlaying.index,
+    val topRatedMoviePage: MovieState = MovieState.DEFAULT,
+    val upcomingMoviePage: MovieState = MovieState.DEFAULT,
+    val popularMoviePage: MovieState = MovieState.DEFAULT,
+    val nowPlayingMoviePage: MovieState = MovieState.DEFAULT,
+
+    val selectedTabIndex: Int = MoviesTabState.NowPlaying.index,
 
     val isLoading: Boolean = false,
     val isError: Boolean = false,
@@ -23,7 +30,7 @@ data class MoviesUiState(
     sealed class PartialState {
         data class AddTopRatedMovies(val topRatedMovies: Movies) : PartialState()
         data class AddUpcomingMovies(val topRatedMovies: Movies) : PartialState()
-        data class AddPopularMovies(val topRatedMovies: Movies) : PartialState()
+        data class AddPopularMovies(val moviePagingData: Movies) : PartialState()
         data class AddNowPlayingMovies(val topRatedMovies: Movies) : PartialState()
         data class ShowErrorDialog(val message: String) : PartialState()
         data class Loading(val show: Boolean) : PartialState()
